902b57914fafd4607a59f52a9ff28d088944b1f3
aditisinha1804 <aditi.sinha.18041998@gmail.com>
2020-04-02 22:05:53 +0530

    Drawer done with corrections

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -38,198 +38,194 @@ class _MyDrawerState extends State<MyDrawer> {
     return Drawer(
       elevation: 20,
       child: Container(
-
+        height: queryData.size.height,
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+          child: ListView(
             children: <Widget>[
 
-              Expanded(
-                child: Container(
+              Container(
 
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        "Home",
+                        style: TextStyle(
+                            fontSize: 17 / queryData.textScaleFactor),
+                      ),
+                      trailing: Icon(Icons.home, color: Colors.black87,),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
                         title: Text(
-                          "Home",
+                          "Dashboard",
                           style: TextStyle(
                               fontSize: 17 / queryData.textScaleFactor),
                         ),
-                        trailing: Icon(Icons.home, color: Colors.black87,),
+                        trailing: Icon(Icons.dashboard, color: Colors.black87,),
                         onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      ListTile(
-                          title: Text(
-                            "Dashboard",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.dashboard),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Category",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.category, color: Colors.black87,),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Attributes",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.edit_attributes,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Product",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.receipt,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Orders",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.shopping_cart,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Orders();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Category",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.category, color: Colors.black87,),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Attributes",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.edit_attributes,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Product",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.receipt,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Orders",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.shopping_cart,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Orders();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
 
-                      ListTile(
-                          title: Text(
-                            "Payment",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.shopping_cart,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Payment();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "UI",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.phone_android,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Register Users",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.person,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Users();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Admob",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.closed_caption,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Push Notification",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.notifications,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
-                      ListTile(
-                          title: Text(
-                            "Configuration",
-                            style: TextStyle(
-                                fontSize: 17 / queryData.textScaleFactor),
-                          ),
-                          trailing: Icon(Icons.content_cut,color: Colors.black87),
-                          onTap: () {
-                            Navigator.pop(context);
-                            var router = new MaterialPageRoute(
-                                builder: (BuildContext context) {
-                                  return Ui();
-                                });
-                            Navigator.of(context).push(router);
-                          }),
+                    ListTile(
+                        title: Text(
+                          "Payment",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.shopping_cart,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Payment();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "UI",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.phone_android,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Register Users",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.person,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Users();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Admob",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.closed_caption,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Push Notification",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.notifications,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
+                    ListTile(
+                        title: Text(
+                          "Configuration",
+                          style: TextStyle(
+                              fontSize: 17 / queryData.textScaleFactor),
+                        ),
+                        trailing: Icon(Icons.content_cut,color: Colors.black87),
+                        onTap: () {
+                          Navigator.pop(context);
+                          var router = new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Ui();
+                              });
+                          Navigator.of(context).push(router);
+                        }),
 
-                    ],
-                  ),
+                  ],
                 ),
               ),
             ],
